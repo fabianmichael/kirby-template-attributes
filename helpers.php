697f8bd7dd5fd05d071377416d69ed12675738b7
Fabian Michael <hallo@fabianmichael.de>
2022-12-02 17:02:58 +0100

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -5,14 +5,11 @@ use FabianMichael\TemplateAttributes\Attributes;
 /**
  * Generates an attributes
  *
- * @param array|null $data A list of attributes as key/value array or an instance of \FabianMichael\TemplateAttributes\Attributes (useful for nested snippets)
- * @param string|null $before An optional string that will be prepended if the result is not empty
- * @param string|null $after An optional string that will be appended if the result is not empty
  * @return \FabianMichael\TemplateAttributes\Attributes
  */
-function attributes(array|Attributes $data = []): Attributes
+function attributes(...$args): Attributes
 {
-	return new Attributes($data);
+	return new Attributes(...$args);
 }
 
 function classes(array|string $classes = []): Attributes
