85414f2a80eeef93243596a677975a5bd45294c6
Daniel Weidner <d.weidner@hks-agentur.de>
2026-03-11 10:17:08 +0100

    feat: extend the default snippet component

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php
 
+use FabianMichael\TemplateAttributes\Attributes;
 use Kirby\Cms\App;
+use Kirby\Template\Snippet;
 
 @include_once __DIR__ . '/vendor/autoload.php';
 
-App::plugin('fabianmichael/template-attributes', []);
+App::plugin('fabianmichael/template-attributes', [
+	'components' => [
+		'snippet' => function (
+			App $kirby,
+			string|array|null $name,
+			array $data = [],
+			bool $slots = false,
+		): Snippet|string {
+			$attributes = new Attributes($data['attr'] ?? []);
+
+			foreach (['id', 'class', 'style'] as $attribute) {
+				if (isset($data[$attribute])) {
+					$attributes->set($attribute, $data[$attribute]);
+				}
+			}
+
+			return Snippet::factory($name, $data + [
+				'attributes' => $attributes,
+			], $slots);
+		}
+	],
+]);
